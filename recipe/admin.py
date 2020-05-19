2713002f43aa4a29c0ea7a9c6ece52f7f409896b
kaizen-cmd <tejasmandre0306@gmail.com>
2020-05-19 22:46:05 +0530

    phase5

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, Tag, BlogPost
+from .models import Recipe, Ingredient, Tag, BlogPost, Category
 
 # Register your models here.
 
 admin.site.register(Recipe)
 admin.site.register(BlogPost)
+admin.site.register(Tag)
+admin.site.register(Category)
